c5a25fadda207099cc241731d41b522560633973
David Way <dway@ie.ibm.com>
2020-10-09 13:41:16 +0100

    Add navigation to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Cluster, Link } from './index';
 
-export default function Header ({ title }) {
+export default function Header ({ title, titleUrl, navigation }) {
   return (
     <Cluster className="c-header" as="header" justify="space-between" spacing="base">
-      <Link className="c-header__title" to="/"><strong>{title}</strong></Link>
+      <Link className="c-header__title" to={titleUrl}><strong>{title}</strong></Link>
       <nav>
         <Cluster as="ul" justify="flex-start" spacing="base">
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
+          {navigation && navigation.map((route) => (
+            <li key={route.title}>
+              <Link to={route.url}>{route.title}</Link>
+            </li>
+          ))}
         </Cluster>
       </nav>
     </Cluster>
